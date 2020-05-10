568f15f9de8b8f24a56f74430def40a52902f977
Benjamin Hoppe <hoppeconnect@gmail.com>
2020-05-10 17:10:59 -0400

    finished second lesson

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -10,12 +10,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
-            Text("UI Design")
-                .font(.title)
-                .fontWeight(.semibold)
+        ZStack {
+            VStack {
+                Spacer()
+            }
+            .frame(width: 300, height: 220)
+            .background(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20)
+            
+            
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                        Text("Placeholder")
+                            .font(.body)
+                            .foregroundColor(Color("accent"))
+
+                    }
+                    Spacer()
+                    Image("Logo1")
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 20)
+                Spacer()
+                Image("Card1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill).frame(width: 300, height: 110, alignment: .top)
+                .clipped()
+                
+            }
+            .frame(width: 340.0, height: 220.0)
+            .background(Color.black.cornerRadius(20))
+            .shadow(radius: 20)
         }
+
             
     }
 }
