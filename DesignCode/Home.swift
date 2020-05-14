445b8bf55daec6af2b311903eab1493c26377797
Benjamin Hoppe <hoppeconnect@gmail.com>
2020-05-14 08:04:12 -0400

    completed lesson 11

--- DesignCode/Home.swift
+++ DesignCode/Home.swift
@@ -40,7 +40,7 @@ struct Home: View {
             .background(Color.white)
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
-            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10.0, y: 0, z: 0))
+            .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
             .offset(y: showProfile ? -450 : 0)
             .scaleEffect(showProfile ? 0.9 : 1)
             .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
