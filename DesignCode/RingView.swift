807f7d05831e8e4d3c26caaddf9f371513ba509f
Benjamin Hoppe <hoppeconnect@gmail.com>
2020-05-23 09:54:37 -0400

    done lesson 23

--- DesignCode/RingView.swift
+++ DesignCode/RingView.swift
@@ -12,10 +12,10 @@ struct RingView: View {
     
     var color1 = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
     var color2 = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
-    var width: CGFloat = 88
-    var height: CGFloat = 88
-    var percent: CGFloat = 44
-    @State var show = true
+    var width: CGFloat = 300
+    var height: CGFloat = 300
+    var percent: CGFloat = 88
+    @Binding var show: Bool
     
     var body: some View {
         let multiplier = width / 44
@@ -27,7 +27,7 @@ struct RingView: View {
                 .frame(width: width, height: height)
             
             Circle()
-                .trim(from: progress, to: 1)
+               .trim(from: show ? progress : 1, to: 1)
                 .stroke(
                     LinearGradient(gradient: Gradient(colors: [Color(color1), Color(color2)]), startPoint: .topTrailing, endPoint: .bottomLeading),
                     style: StrokeStyle(lineWidth: 5 * multiplier, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20,0], dashPhase: 0)
@@ -35,11 +35,14 @@ struct RingView: View {
             .rotationEffect(Angle(degrees: 90))
             .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
             .frame(width: width, height: height)
-                .shadow(color: Color(color2).opacity(0.1), radius: 3 * multiplier, x: 0, y: 3 * multiplier)
+            .shadow(color: Color(color2).opacity(0.1), radius: 3 * multiplier, x: 0, y: 3 * multiplier)
             
             Text("\(Int(percent))%")
                 .font(.system(size: 14 * multiplier))
                 .fontWeight(.bold)
+                .onTapGesture {
+                    self.show.toggle()
+            }
         }
         
     }
@@ -47,6 +50,6 @@ struct RingView: View {
 
 struct RingView_Previews: PreviewProvider {
     static var previews: some View {
-        RingView()
+        RingView(show: .constant(true))
     }
 }
