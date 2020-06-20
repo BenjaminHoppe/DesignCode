7008cb4cf990e7abd1c1e993d6dbafbb72fdc9b1
Benjamin Hoppe <hoppeconnect@gmail.com>
2020-06-20 17:43:55 -0400

    completed lesson 34

---
+++ DesignCode/Data.swift
@@ -0,0 +1,30 @@
+//
+//  Data.swift
+//  DesignCode
+//
+//  Created by Benjamin Hoppe on 2020-06-20.
+//  Copyright © 2020 Benjamin Hoppe. All rights reserved.
+//
+
+import SwiftUI
+
+struct Post: Codable, Identifiable {
+    let id = UUID()
+    var title: String
+    var body: String
+}
+
+class Api {
+    func getPosts(completion: @escaping ([Post]) -> ()) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            let posts = try! JSONDecoder().decode([Post].self, from: data!)
+            
+            DispatchQueue.main.async {
+                completion(posts)
+            }
+        }
+        .resume()
+    }
+}
